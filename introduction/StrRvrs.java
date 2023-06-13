66733464dfb8ff0c0720d86506b31a3ddee1241b
Connor Sheets <connorsheets32@gmail.com>
2023-06-13 09:55:54 -0600

    reverse string

---
+++ introduction/StrRvrs.java
@@ -0,0 +1,20 @@
+package introduction;
+
+class Solution  {
+    public String StrRvrs(String s){
+        char[] chars = s.toCharArray();
+        int l = 0, r = s.length() -1;
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)){
+                char temp = chars[l];
+                chars[l] = chars[r];
+                chars[r] = temp;
+                l ++;
+                r--;
+            }
+        }
+        return new String(chars);
+
+    }
+    
+}
