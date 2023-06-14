4807191bf4849a2988e762d2eb919c7fcb5a5e4e
Connor Sheets <connorsheets32@gmail.com>
2023-06-14 15:01:04 -0600

    validrom

---
+++ introduction/IsPalindrome.java
@@ -0,0 +1,22 @@
+package introduction;
+class Solution {
+    public boolean isPalindrome(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+        while (l < r) {
+            char lChar = Character.toLowerCase(s.charAt(l));
+            char rChar = Character.toLowerCase(s.charAt(r));
+            if (!Character.isLetterOrDigit(lChar)){
+                l++;
+            } else if (!Character.isLetterOrDigit(rChar)){
+                r--;
+            } else if (lChar!= rChar){
+                return false;
+            }else {
+                l++;
+                r--;
+            }
+        }
+        return true;
+    }
+}
